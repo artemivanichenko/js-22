f1cd4ddb830dec09327a3542ac8a81430524cb1a
Artem <ivanichenko.ua@gmail.com>
2023-01-22 10:10:26 +0200

    dsaa

---
+++ js/test.js
@@ -0,0 +1,23 @@
+// const productName = 'Droid';
+// const pricePerItem = 3500;
+
+// let message = `You picked ${productName} price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// 1. Объявление функции multiply
+function multiply() {
+    // Тело функции
+    console.log("multiply function invocation");
+  }
+  
+  // 2. Вызовы функции multiply
+  multiply(); // multiply function invocation
+  multiply(); // multiply function invocation
+  multiply(); // multiply function invocation
